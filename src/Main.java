1847f2deb4d1dc721ada7e24052529f56154f6b5
lifanova <veralifanovaa@gmail.com>
2021-03-06 15:52:54 +0300

    Project files are added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int current = 2_000_000_000;
+        int temp = 500_000_000;
+        int result = current + temp;
+
+        System.out.println(result);
+    }
+}
